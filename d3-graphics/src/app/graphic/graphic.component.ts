f312745f51b159fa5a09fec35773b509bb2a3f8c
DiegoVeronezi <diegoveronezi70@gmail.com>
2019-01-24 13:54:20 -0200

    feat: init graphic component

---
+++ d3-graphics/src/app/graphic/graphic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graphic',
+  templateUrl: './graphic.component.html',
+  styleUrls: ['./graphic.component.css']
+})
+export class GraphicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
